daf0ea8a1081b603516394d17b528f678fa73c21
ShekharPareek <shekhar.pareek@sunarctechnologies.com>
2025-03-15 19:29:37 +0530

    code push

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'health-tracker-v1';
+
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/src/main.tsx',
+  '/src/index.css',
+  '/icons/water.svg',
+  '/icons/steps.svg',
+  '/icons/icon-192.svg',
+  '/icons/icon-512.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
